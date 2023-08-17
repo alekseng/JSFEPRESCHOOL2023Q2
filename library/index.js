cb72468b57514797e3ac2b4c1903fc00f5dca8d5
alekseng <alexvdche@mail.ru>
2023-08-17 06:04:41 +0300

    feat: make a carousel slider in about section

--- library/index.js
+++ library/index.js
@@ -118,16 +118,116 @@ function changeLogoText() {
 
 changeLogoText()
 
-console.log(`Все требования к работе выполнены = 50 баллов
+function slider() {
+  const slider = document.querySelector('.slides__container');
+  const dots = document.querySelectorAll('.slider__dot');
+  const slideItemInd = document.querySelectorAll('.slides__item');
+  const slideItem = document.querySelector('.slides__img');
+  const leftBtn = document.querySelector('.left-arrow');
+  const rightBtn = document.querySelector('.right-arrow');
 
-1.Вёрстка соответствует макету. Ширина экрана 768px +26
-2.Ни на одном из разрешений до 640px включительно не появляется горизонтальная полоса прокрутки.
-  Весь контент страницы при этом сохраняется: не обрезается и не удаляется +12
-3.На ширине экрана 768рх реализовано адаптивное меню +12 
-  (бургер-меню появляется на ширине 1024px, как рекомендовано в задании):
-  - при нажатии на бургер-иконку плавно появляется адаптивное меню +4
-  - при нажатии на крестик, или на область вне меню, адаптивное меню плавно скрывается, уезжая за экран +4
-  - ссылки в адаптивном меню работают, обеспечивая плавную прокрутку по якорям при нажатии, а само адаптивное меню при этом плавно скрывается +2
-  - размеры открытого бургер-меню соответствуют макету +2
-4. Сделан адаптив до 320px
-`)
+  let countWidth = 0;
+  let left = slideItem.clientWidth + 25;
+  let right = slideItem.clientWidth + 25;
+  let index = 1;
+  addEventListener('resize', () => {
+    slider.style.transform = `translate3d(0px, 0px, 0px)`;
+    left = slideItem.clientWidth + 25;
+    right = slideItem.clientWidth + 25;
+    countWidth = 0;
+    leftBtn.disabled = true
+    rightBtn.disabled = false
+    for (let i = 0; i < dots.length; i++) {
+      dots[i].classList.remove('_active')
+      dots[i].classList.remove('_non-active')
+      dots[0].classList.add('_active')
+      dots[0].classList.add('_non-active')
+    }
+  })
+  dots.forEach((el, ind) => {
+    el.addEventListener('click', function () {
+      countWidth = (slideItem.clientWidth * ind) + (25 * ind);
+      index = ind + 1;
+      for (let i = 0; i < dots.length; i++) {
+        dots[i].classList.remove('_active')
+        dots[i].classList.remove('_non-active')
+      }
+      if (el.dataset.index) {
+        slider.style.transform = `translate3d(-${(slideItem.clientWidth * ind) + (25 * ind)}px, 0px, 0px)`;
+        el.classList.add('_active')
+        el.classList.add('_non-active')
+        rightBtn.disabled = false
+        leftBtn.disabled = false
+      }
+      if (index == 1) {
+        leftBtn.disabled = true
+        rightBtn.disabled = false
+      }
+      if (index == dots.length) {
+        rightBtn.disabled = true
+        leftBtn.disabled = false
+      }
+    })
+  })
+  leftBtn.addEventListener('click', function () {
+    for (let i = 0; i < dots.length; i++) {
+      dots[i].classList.remove('_active')
+      dots[i].classList.remove('_non-active')
+    }
+    if (countWidth > 0) {
+      slider.style.transform += `translate3d(${left}px, 0px, 0px)`;
+      countWidth -= left;
+      index--
+      for (let dot of dots) {
+        if (dot.dataset.index == index) {
+          dot.classList.add('_active')
+          dot.classList.add('_non-active')
+        }
+      }
+    }
+    if (index == 1) {
+      leftBtn.disabled = true
+    } else if (index > 1) {
+      rightBtn.disabled = false
+    }
+  })
+
+  rightBtn.addEventListener('click', function () {
+    for (let i = 0; i < dots.length; i++) {
+      dots[i].classList.remove('_active')
+      dots[i].classList.remove('_non-active')
+    }
+    if (countWidth < (right * slideItemInd.length - 1) - right) {
+      countWidth += right;
+      slider.style.transform += `translate3d(-${right}px, 0px, 0px)`;
+      index++
+      for (let dot of dots) {
+        if (dot.dataset.index == index) {
+          dot.classList.add('_active')
+          dot.classList.add('_non-active')
+        }
+      }
+    }
+    if (index == dots.length) {
+      rightBtn.disabled = true
+    } else if (index < dots.length) {
+      leftBtn.disabled = false
+    }
+  })
+}
+
+slider()
+
+// console.log(`Все требования к работе выполнены = 50 баллов
+
+// 1.Вёрстка соответствует макету. Ширина экрана 768px +26
+// 2.Ни на одном из разрешений до 640px включительно не появляется горизонтальная полоса прокрутки.
+//   Весь контент страницы при этом сохраняется: не обрезается и не удаляется +12
+// 3.На ширине экрана 768рх реализовано адаптивное меню +12
+//   (бургер-меню появляется на ширине 1024px, как рекомендовано в задании):
+//   - при нажатии на бургер-иконку плавно появляется адаптивное меню +4
+//   - при нажатии на крестик, или на область вне меню, адаптивное меню плавно скрывается, уезжая за экран +4
+//   - ссылки в адаптивном меню работают, обеспечивая плавную прокрутку по якорям при нажатии, а само адаптивное меню при этом плавно скрывается +2
+//   - размеры открытого бургер-меню соответствуют макету +2
+// 4. Сделан адаптив до 320px
+// `)
