35899249a01984300adab287fe93b56126798a3f
alekseng <alexvdche@mail.ru>
2023-09-27 09:18:56 +0300

    feat: add search

--- image-galery/js/index.js
+++ image-galery/js/index.js
@@ -1,7 +1,16 @@
-let query = 'cats';
+const search = document.getElementById('search');
+let query;
+search.value.length > '0' ? query = search.value : query = 'cheetah';
 const ID = 'h-JB_S_5wn16PF8uv5eRY9eWZlsPEzZk9vddRNpQjYo';
 
-async function getData(renderImages) {
+function changeValue() {
+  query = search.value;
+};
+
+search.addEventListener('change', changeValue);
+search.addEventListener('change', getData);
+
+async function getData() {
   const url = `https://api.unsplash.com/search/photos?query=${query}&per_page=30&orientation=landscape&client_id=${ID}`;
   const res = await fetch(url);
   const data = await res.json();
@@ -9,11 +18,13 @@ async function getData(renderImages) {
 };
 
 function renderImages(data) {
+  console.log(data);
   const item = document.querySelector('.content-container');
-  data.forEach(el => {
+  if(search.value.length > '0') {item.replaceChildren()};
+  data.map(el => {
     const template = document.createElement('template');
     template.innerHTML = `<div class="content-container__item"><img src="${el.urls.small}" alt="img">`;
     item.appendChild(template.content);
   });
 };
-getData(renderImages);
+getData();
