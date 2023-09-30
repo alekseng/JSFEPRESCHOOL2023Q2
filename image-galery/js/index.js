1efde408e825ee564ede1f6dffe6550141b65808
alekseng <alexvdche@mail.ru>
2023-09-30 09:19:59 +0300

    feat: add a dark theme refactor: redesigned the function of sending a request

--- image-galery/js/index.js
+++ image-galery/js/index.js
@@ -1,14 +1,21 @@
 const search = document.getElementById('search');
-let query;
-search.value.length > '0' ? query = search.value : query = 'cheetah';
+const clearBtn = document.querySelector('.clear');
+const searchBtn = document.querySelector('.search');
+const content = document.querySelector('.content-container');
+const overlay = document.querySelector('.overlay');
+const theme = document.querySelector('.theme');
+const searchForm = document.querySelector('.search-container');
+
+let query = 'cheetah';
 const ID = 'h-JB_S_5wn16PF8uv5eRY9eWZlsPEzZk9vddRNpQjYo';
 
-function changeValue() {
-  query = search.value;
-};
-
-search.addEventListener('change', changeValue);
-search.addEventListener('change', getData);
+searchBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (search.value.length != '0') {
+    query = search.value;
+    getData();
+  }
+});
 
 async function getData() {
   const url = `https://api.unsplash.com/search/photos?query=${query}&per_page=30&orientation=landscape&client_id=${ID}`;
@@ -29,9 +36,6 @@ function renderImages(data) {
 };
 getData();
 
-const clearBtn = document.querySelector('.clear');
-const searchBtn = document.querySelector('.search');
-
 clearBtn.addEventListener('click', clearInput);
 
 search.addEventListener('input', function () {
@@ -47,9 +51,6 @@ function clearInput() {
   clearBtn.classList.remove('_show');
 };
 
-const content = document.querySelector('.content-container');
-const overlay = document.querySelector('.overlay');
-
 function load() {
   const images = content.querySelectorAll('div');
   images.forEach((el) => {
@@ -60,3 +61,40 @@ function load() {
     });
   });
 };
+
+searchForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+});
+
+if (localStorage.getItem('isDark') == 'false') {
+  document.documentElement.style.setProperty('--main-color', 'white');
+  document.documentElement.style.setProperty('--text-color', 'black');
+  document.documentElement.style.setProperty('--svg-color', 'black');
+  document.documentElement.style.setProperty('--shadow-color', 'black');
+  theme.classList.remove('_active');
+} else {
+  document.documentElement.style.setProperty('--main-color', 'black');
+  document.documentElement.style.setProperty('--text-color', 'orange');
+  document.documentElement.style.setProperty('--svg-color', 'orange');
+  document.documentElement.style.setProperty('--shadow-color', 'orange');
+  theme.classList.add('_active');
+}
+
+theme.addEventListener('click', () => {
+  if (localStorage.getItem('isDark') == 'false') {
+    document.documentElement.style.setProperty('--main-color', 'black');
+    document.documentElement.style.setProperty('--text-color', 'orange');
+    document.documentElement.style.setProperty('--svg-color', 'orange');
+    document.documentElement.style.setProperty('--shadow-color', 'orange');
+    localStorage.setItem('isDark', true);
+    theme.classList.add('_active');
+  }
+  else {
+    document.documentElement.style.setProperty('--main-color', 'white');
+    document.documentElement.style.setProperty('--text-color', 'black');
+    document.documentElement.style.setProperty('--svg-color', 'black');
+    document.documentElement.style.setProperty('--shadow-color', 'black');
+    localStorage.setItem('isDark', false);
+    theme.classList.remove('_active');
+  }
+});
