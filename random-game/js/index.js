0a3b28de94ffbf7836be83511f8670a38f3bc6d1
alekseng <alexvdche@mail.ru>
2023-10-04 11:18:14 +0300

    feat: add movement for the player

--- random-game/js/index.js
+++ random-game/js/index.js
@@ -9,6 +9,25 @@ class Tank {
   y = 0;
   width = 30;
   height = 30;
+  directions = {
+    up: {
+      y: -1,
+      pressed: false,
+    },
+    down: {
+      y: 1,
+      pressed: false,
+    },
+    left: {
+      x: -1,
+      pressed: false,
+    },
+    right: {
+      x: 1,
+      pressed: false,
+    }
+  };
+
   constructor(x, y) {
     this.x = x;
     this.y = y;
@@ -16,15 +35,70 @@ class Tank {
 
   draw() {
     ctx.fillStyle = 'red';
-    ctx.fillRect(this.x, this.y, this.width , this.height);
+    ctx.fillRect(this.x, this.y, this.width, this.height);
   };
 };
 
 const player = new Tank(x = 245, y = 245);
 
+window.addEventListener('keydown', (e) => {
+  if (e.keyCode == 37) {
+    player.directions.right.pressed = true;
+  }
+  else if (e.keyCode == 38) {
+    player.directions.down.pressed = true;
+  }
+  else if (e.keyCode == 39) {
+    player.directions.left.pressed = true;
+  }
+  else if (e.keyCode == 40) {
+    player.directions.up.pressed = true;
+  };
+});
+
+window.addEventListener('keyup', (e) => {
+  if (e.keyCode == 37) {
+    player.directions.right.pressed = false;
+  }
+  else if (e.keyCode == 38) {
+    player.directions.down.pressed = false;
+  }
+  else if (e.keyCode == 39) {
+    player.directions.left.pressed = false;
+  }
+  else if (e.keyCode == 40) {
+    player.directions.up.pressed = false;
+  };
+});
+
 function animation() {
   window.requestAnimationFrame(animation);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   player.draw();
-}
+
+  if (player.directions.right.pressed) {
+    player.directions.up.pressed = false;
+    player.directions.down.pressed = false;
+    player.directions.right.x = 1;
+    player.x += player.directions.left.x;
+  };
+  if (player.directions.left.pressed) {
+    player.directions.up.pressed = false;
+    player.directions.down.pressed = false;
+    player.directions.left.x = -1;
+    player.x += player.directions.right.x;
+  };
+  if (player.directions.up.pressed) {
+    player.directions.right.pressed = false;
+    player.directions.left.pressed = false;
+    player.directions.up.y = -1;
+    player.y += player.directions.down.y;
+  };
+  if (player.directions.down.pressed) {
+    player.directions.right.pressed = false;
+    player.directions.left.pressed = false;
+    player.directions.down.y = 1;
+    player.y += player.directions.up.y;
+  };
+};
 animation();
