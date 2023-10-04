5059ff7aa2c17053c61e9842ad4a7ee647440fc4
alekseng <alexvdche@mail.ru>
2023-10-04 05:32:10 +0300

    feat: add player in game

--- random-game/js/index.js
+++ random-game/js/index.js
@@ -3,3 +3,28 @@ const ctx = canvas.getContext('2d');
 
 canvas.width = 520;
 canvas.height = 520;
+
+class Tank {
+  x = 0;
+  y = 0;
+  width = 30;
+  height = 30;
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  draw() {
+    ctx.fillStyle = 'red';
+    ctx.fillRect(this.x, this.y, this.width , this.height);
+  };
+};
+
+const player = new Tank(x = 245, y = 245);
+
+function animation() {
+  window.requestAnimationFrame(animation);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  player.draw();
+}
+animation();
